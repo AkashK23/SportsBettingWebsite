e5980f6083a72103c54da017bdc35df816137e22
AkashK23 <34080339+AkashK23@users.noreply.github.com>
2019-12-10 17:56:02 -0500

    Revert "idk"
    This reverts commit 05911c01a30e0fca3744780257497e1ee30bbcd3, reversing
    changes made to ce8302bb3f6533ae6b9dc89db469ffd210dc1249.

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ export async function getOdds(id) {
             mkt: 'h2h' // h2h | spreads | totals
         }
     });
-    console.log('Remaining requests', odds.headers['x-requests-remaining'])
     return odds.data.data;
     // .then(response => {
     //     // odds_json['data'] contains a list of live and 
