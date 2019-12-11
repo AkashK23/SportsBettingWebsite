b9b77f73fffedf63a2ce7c626811e7bf3cda2c2d
ramvadla8 <vadlamudi.sriram@gmail.com>
2019-12-10 23:24:33 -0500

    debounce test

---
+++ debounce.js
@@ -0,0 +1,11 @@
+function throttle(f, delay){
+    var timer = null;
+    return function(){
+        var context = this, args = arguments;
+        clearTimeout(timer);
+        timer = window.setTimeout(function(){
+            f.apply(context, args);
+        },
+        delay || 500);
+    };
+}
